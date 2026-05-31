87b48c0aa367dbd0773e8e268008a696ae6a87f9
Usman <heart.uk6777@gmail.com>
2026-06-01 00:00:36 +0500

    Updated Hugging Face API URL

--- backend/services/embedding_service.py
+++ backend/services/embedding_service.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Hugging Face Free API URL exact same model ke liye
-API_URL = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+API_URL = "https://router.huggingface.co/hf-inference/models/sentence-transformers/all-MiniLM-L6-v2"
 
 # Token aap ki .env file se aayega
 headers = {"Authorization": f"Bearer {os.getenv('HF_API_KEY')}"}
